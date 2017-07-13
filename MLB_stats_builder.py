ef669035b98b6a053f7cdc2be69461b86f814629
Billy Castelli (Will) <billycastelli@gmail.com>
2017-07-12 22:15:08 -0700

    Update MLB_stats_builder.py

--- MLB_stats_builder.py
+++ MLB_stats_builder.py
@@ -1,4 +1,7 @@
 # MLB Stats builder
+#     The information used here was obtained free of
+#     charge from and is copyrighted by Retrosheet.  Interested
+#     parties may contact Retrosheet at "www.retrosheet.org".
 
 
 import json
