b939a209f35cf61b4622467132881a2ff5c3f838
Billy Castelli (Will) <billycastelli@gmail.com>
2017-07-29 18:27:47 -0700

    Update mlb_stats_input.py

--- mlb_stats_input.py
+++ mlb_stats_input.py
@@ -3,15 +3,18 @@
 #     charge from and is copyrighted by Retrosheet.  Interested
 #     parties may contact Retrosheet at "www.retrosheet.org".
 
+#### MLB Stats
+
+#Stats provided by www.retrosheet.org
+
 import MLB_stats_builder as mlbb
 
 def inputName():
     name = input('Enter the name of an MLB player: ')
     return name
 
-def get_stats():
+def getStats(player:str):
     print()
-    player = inputName()
     last_name_url = mlbb.last_name_url(player)
     #print(last_name_url)
     last_name_source = mlbb.get_source(last_name_url)
@@ -20,11 +23,15 @@ def get_stats():
     player_url = mlbb.get_player_url(player,last_name_source)
     #print(mlbb.get_source(player_url))
     stats = mlbb.parse_source(mlbb.get_source(player_url))
+    for line in stats:
+        print(line)
 
 if __name__ == "__main__":
-    while True:
-        get_stats()
+    name = inputName()
+    getStats(name)
+   
     
+#put stats in dict so they are stored in the program for access
 
     
     
